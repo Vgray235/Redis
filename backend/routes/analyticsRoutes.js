f542869b6dad1f6fc01c62c017cef81b5a48237c
VasuGrayMatter <vasu.singhal@graymatter.co.in>
2025-08-13 04:02:30 +0530

    Delete backend directory

--- backend/routes/analyticsRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { ensureAuth } from "../middleware/auth.js";
-import { getAnalytics } from "../controllers/analyticsController.js";
-
-const router = express.Router();
-router.get("/", ensureAuth, getAnalytics);
-export default router;
